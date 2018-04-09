e5d8c655feca923032ecaf97a1a3d71ae7337afc
Alex Saiko <solcmdr@gmail.com>
2018-04-09 14:25:12 +0300

    vendor: pure: Add Hammerhead (LGE Nexus 5) device
    Add a new device to be supported by an Unofficial PureNexus Project.
    
    Change-Id: I192d151cb92119cb1b6c4b5949a899a5facfca3f
    Signed-off-by: Alex Saiko <solcmdr@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,7 @@
 add_lunch_combo angler-user
 add_lunch_combo bullhead-user
 add_lunch_combo dragon-user
+add_lunch_combo hammerhead-user
 add_lunch_combo marlin-user
 add_lunch_combo sailfish-user
 add_lunch_combo shamu-user
